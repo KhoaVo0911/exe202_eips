8d1a7a9f1cf4b0c0210e65df7ae752b112598272
Minh Đinh <minhdq2512@gmail.com>
2024-05-13 15:40:05 +0700

    update-ui

--- src/admin/Navbar.jsx
+++ src/admin/Navbar.jsx
@@ -11,30 +11,34 @@ export default function Navbar() {
 
   return (
     <div>
-      <nav
-        className="bg-[#A8DADC] border-gray-200 px-2 sm:px-4 rounded dark:bg-gray-900 shadow-md"
-      >
+      <nav className="bg-[#A8DADC] border-gray-200 px-2 sm:px-4 rounded dark:bg-gray-900 shadow-md">
         <div className="max-w-[1500px] flex flex-wrap items-center justify-between mx-auto">
           <Link to="/" className="flex items-center">
             {/* Your logo here */}
           </Link>
           <div className="flex items-center">
             <NavLink
-              style={({ isActive }) => (isActive ? { color: "#60a5fa" } : undefined)}
+              style={({ isActive }) =>
+                isActive ? { color: "#60a5fa" } : undefined
+              }
               to="/Admin"
               className="text-black mr-4"
             >
               <h6>Events</h6>
             </NavLink>
             <NavLink
-              style={({ isActive }) => (isActive ? { color: "#60a5fa" } : undefined)}
+              style={({ isActive }) =>
+                isActive ? { color: "#60a5fa" } : undefined
+              }
               to="/AccountAdmin"
               className="text-black mr-4"
             >
               <h6>Accounts</h6>
             </NavLink>
             <NavLink
-              style={({ isActive }) => (isActive ? { color: "#60a5fa" } : undefined)}
+              style={({ isActive }) =>
+                isActive ? { color: "#60a5fa" } : undefined
+              }
               to="/SettingAdmin"
               className="text-black"
             >
@@ -43,12 +47,8 @@ export default function Navbar() {
           </div>
           <div className="hidden w-full lg:block md:w-auto" id="navbar-default">
             <ul className="flex mt-4">
-              <img
-                src={userObject.image}
-                className="-mt-[10px] h-12 w-12 rounded-full"
-              />
               <div className="ml-4">
-                <p className="font-bold text-2xl">{userObject.name}</p>
+                <p className="font-bold text-2xl"></p>
               </div>
             </ul>
           </div>
