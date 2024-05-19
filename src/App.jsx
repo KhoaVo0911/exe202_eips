f9015cc9fc8919713b5dc03c99c7d42f91ba92b6
Võ Đăng Khoa <133868589+KhoaVo0911@users.noreply.github.com>
2024-05-19 15:17:35 +0700

    add Slider

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Login from "./pages/login/login";
 import Product from "./pages/product/Product";
+import ImageSlider from "./pages/Slider";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={<Login />} /> */}
         <Route path="/Product" element={<Product />} />
+        <Route path="/slider" element={<ImageSlider />} />
       </Routes>
     </Router>
   );
