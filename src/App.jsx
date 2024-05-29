135d7f24f9ed6eeeadccfe59a4cccb93c608b2bc
Minh Đinh <minhdq2512@gmail.com>
2024-05-29 11:49:08 +0700

    new-ui

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,11 @@ import Create from "./pages/product/Create";
 import Cart from "./pages/product/Cart";
 import ListOrdered from "./pages/product/ListOrdered";
 import Homepage from "./admin/home/Homepage";
-import CardList from "./admin/Card/CardList";
+
+import ListShop from "./admin/home/ListShop";
+import ListAccounts from "./admin/home/ListAccounts";
+import ListCards from "./admin/home/ListCards";
+import ListOrders from "./admin/home/ListOrders";
 
 function App() {
   return (
@@ -15,6 +19,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Homepage />} />
+        <Route path="/ListShop" element={<ListShop />} />
+        <Route path="/ListAccounts" element={<ListAccounts />} />
+        <Route path="/ListCards" element={<ListCards />} />
+        <Route path="/ListOrders" element={<ListOrders/>} />
 
         <Route path="/Product" element={<Product />} />
         <Route path="/Payment" element={<Payment />} />
